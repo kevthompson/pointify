43013dcc61f0242ef3c21f8a5c64747300afb290
Kevin <kevthompson97@gmail.com>
2018-06-16 14:37:39 -0500

    asdadsf

--- pointify.js
+++ pointify.js
@@ -7,26 +7,5 @@ function setup() {
 }
 
 function draw() {
-    fetch('https://www.reddit.com/r/RoomPorn.json')
-        .then(res => res.json())
-        .then(res => res.data.children)
-        .then(res => res.map(post => ({
-            author: post.data.author,
-            link: post.data.url,
-            img: post.data.preview.images[0].source.url,
-        })))
-        .then(res => res.map(render))
-        .then(res => console.log(res))
 
-    const app = document.querySelector('#app');
-
-    const render = post => {
-        const node = document.createElement('div');
-        node.innerHTML = `
-          <a href="${post.link}">
-            <img src="${post.img}"/>
-          </a>`;
-        app.appendChild(node);
-        return post
-    }
 }
